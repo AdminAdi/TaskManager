0479702bbfc17c78a4b14c20f4716d291c9073ad
Aditya <itsmeaditya3254@gmail.com>
2024-12-13 23:02:19 +0530

    login updation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from './pages/Login'
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
+import Users from './pages/Users';
+import Trash from './pages/Trash';
+import TaskDetails from './pages/TaskDetails';
+import { Toaster } from "sonner";
+
+
+function Layout() {
+  const user = ""
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        {/* Sidebar */}
+      </div>
+      {/*Mobile Sidebar */}
+
+      <div className='flex-1 overflow-y-auto'>
+        {/* Navbar */}
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
+
+        </div>
+
+      </div>
+
+    </div>
+
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users />} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+
+        </Route>
+
+        <Route path='/log-in' element={<Login />} />
+
+      </Routes>
+      <Toaster richColors />
+    </main>
   )
+
+
+
+
+
 }
 
 export default App
